2cfb8b9533252acb1ba972d09e929bf9aaa2264c
John-Bruce Noad <johnnoad04@gmail.com>
2023-05-14 19:42:23 +0200

    Added Iframe Example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <section id="home">
-    <div class="container bg-danger">
+    <div class="container">
       <div class="row">
         <div class="col-6">
 
@@ -8,7 +8,7 @@
         </div>
         <div class="col-6">
           Welcome to my Portfolio
-          <!-- Buttons Here -->
+          <iframe src="https://www.google.com/webhp?igu=1" class="flex-grow border-primary border border-3" id="chrome-screen" width="700" height="530" frameborder="0" title="Chrome"></iframe>          <!-- Buttons Here -->
           <!-- They Link to Games/Fun Pages -->
           <!-- These act as Project examples, to showcase my skills -->
         </div>
@@ -38,4 +38,5 @@ export default {
 #home .container {
   height: 500px;
 }
+
 </style>
