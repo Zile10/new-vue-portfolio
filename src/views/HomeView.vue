18ffff4e1932eabdda50378a18e9551e07d95079
unknown <johnnoad04@gmail.com>
2023-05-17 16:34:10 +0200

    Minor home page thing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,8 @@
   <section id="home">
     <div class="container">
       <div class="row">
-        <div class="col-6">
-
-          <img src="" alt="...">
-        </div>
-        <div class="col-6">
-          Welcome to my Portfolio
-          <iframe src="https://www.google.com/webhp?igu=1" class="flex-grow border-primary border border-3" id="chrome-screen" width="700" height="530" frameborder="0" title="Chrome"></iframe>          <!-- Buttons Here -->
-          <!-- They Link to Games/Fun Pages -->
-          <!-- These act as Project examples, to showcase my skills -->
+        <div class="col-12">
+          <h1>John-Bruce Noad</h1>
         </div>
       </div>
     </div>
