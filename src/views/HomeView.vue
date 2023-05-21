29136e7a22038565fe257344e4b9ef2aae47df05
John-Bruce Noad <johnnoad04@gmail.com>
2023-05-21 19:23:29 +0200

    Home Page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,60 @@
 <template>
   <section id="home">
-    <div class="container d-flex flex-column justify-content-center">
-      <h1>John-Bruce Noad</h1>
+    <div
+      class="container d-flex flex-column justify-content-center align-items-center"
+    >
+      <div class="content">
+        <h1>John-Bruce Noad</h1>
+        <div class="controls">
+          <form class="d-flex" role="search">
+            <input
+              class="form-control rounded-5 border-3"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <!-- <button class="btn btn-outline-success" type="submit">Search</button> -->
+          </form>
+          <div class="row">
+            <div class="col-2 p-1 pt-2">
+              <div class="card">
+                <img width="48" src="https://img.icons8.com/material-outlined/48/image.png" alt="image" class="m-auto"/>
+                <p>Shortcut</p>
+              </div>
+            </div>
+            <div class="col-2 p-1 pt-2">
+              <div class="card">
+                <img width="48" src="https://img.icons8.com/material-outlined/48/image.png" alt="image" class="m-auto"/>
+                <p>Shortcut</p>
+              </div>
+            </div>
+            <div class="col-2 p-1 pt-2">
+              <div class="card">
+                <img width="48" src="https://img.icons8.com/material-outlined/48/image.png" alt="image" class="m-auto"/>
+                <p>Shortcut</p>
+              </div>
+            </div>
+            <div class="col-2 p-1 pt-2">
+              <div class="card">
+                <img width="48" src="https://img.icons8.com/material-outlined/48/image.png" alt="image" class="m-auto"/>
+                <p>Shortcut</p>
+              </div>
+            </div>
+            <div class="col-2 p-1 pt-2">
+              <div class="card">
+                <img width="48" src="https://img.icons8.com/material-outlined/48/image.png" alt="image" class="m-auto"/>
+                <p>Shortcut</p>
+              </div>
+            </div>
+            <div class="col-2 p-1 pt-2">
+              <div class="card">
+                <img width="48" src="https://img.icons8.com/material-outlined/48/image.png" alt="image" class="m-auto"/>
+                <p>Shortcut</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -10,22 +63,26 @@
 // @ is an alias to /src
 
 export default {
-  name: 'HomeView',
-  components: {
-
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
 
-<style>
+<style scoped>
 #home {
   display: flex;
   flex-direction: column;
   justify-content: center;
-
 }
 #home .container {
-  height: 500px;
+  min-height: 500px;
 }
 
+h1 {
+  font-weight: bolder;
+  color: #333;
+}
+.container .controls {
+  width: 600px;
+}
 </style>
